5244cbf63c93ae9ddbc11fc6070e3b7199ad2351
Karan Dhillon <karanpal.dhillon@gmail.com>
2018-09-09 22:52:51 +0530

    initial commit

---
+++ tomato.js
@@ -0,0 +1,65 @@
+let time = 1 * 60;
+let interval = null;
+let minuteElem = document.querySelector('#minutes');
+let secondElem = document.querySelector('#seconds');
+let startElem = null;
+let pauseElem = null;
+let resetElem = null;
+let sound = new Howl({
+  src: 'tick.mp3'
+});
+function start($event) {
+  startElem.setAttribute('disabled', 'disabled');
+  interval = setInterval(startCountdown, 1000);
+  pauseElem.removeAttribute('disabled');
+  resetElem.removeAttribute('disabled');
+}
+window.addEventListener('load', () => {
+  minuteElem.innerHTML = padding(Math.floor(time / 60));
+  secondElem.innerHTML = padding(time - 60 * Math.floor(time / 60));
+  startElem = document.querySelector('#start');
+  pauseElem = document.querySelector('#pause');
+  resetElem = document.querySelector('#reset');
+  // interval = setInterval(startCountdown, 1000);
+});
+
+function startCountdown() {
+  if (time === 0) clearInterval(interval);
+  let minutes = padding(Math.floor(time / 60));
+  let seconds = padding(time - 60 * minutes);
+  minuteElem.innerHTML = minutes;
+  secondElem.innerHTML = seconds;
+  time -= 1;
+  sound.play();
+}
+
+function padding(num) {
+  if (num < 10) {
+    return `0${num}`;
+  }
+  return `${num}`;
+}
+
+function pause() {
+  clearInterval(interval);
+  startElem.removeAttribute('disabled');
+  pauseElem.setAttribute('disabled', 'disabled');
+}
+
+function reset() {
+  time = 25 * 60;
+  clearInterval(interval);
+  minuteElem.innerHTML = padding(Math.floor(time / 60));
+  secondElem.innerHTML = padding(time - 60 * Math.floor(time / 60));
+}
+
+document.querySelector('#twenty-five-min').addEventListener('click', () => {
+  time = 25 * 60;
+  start();
+});
+
+function startCustomTimer($event, cTime) {
+  time = cTime * 60;
+  clearInterval(interval);
+  start($event);
+}
